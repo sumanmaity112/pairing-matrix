baaf132d2249fc1bc7267eab759a74b4ea5ab323
sumanmaity112 <suman.maity112@gmail.com>
2022-05-07 21:21:18 +0530

    feat: Move config as side bar instead of top bar

--- frontend-app/src/components/PairingMatrixPage.vue
+++ frontend-app/src/components/PairingMatrixPage.vue
@@ -1,35 +1,36 @@
 <template>
   <div class="pairing-matrix-page">
     <h2 class="pairing-matrix-title">Pairing Matrix</h2>
-    <div class="pairing-matrix-config-container">
-      <section class="pairing-matrix-config">
-        <h4>Pull new data from github:</h4>
-        <input type="radio" v-model="pullData" value="true" />Yes
-        <input type="radio" v-model="pullData" value="false" />No
-      </section>
-      <section class="pairing-matrix-config">
-        <h4>For last days:</h4>
-        <input type="number" v-model="sinceDays" />
-      </section>
-      <section class="pairing-matrix-config">
-        <h4>Aggregate By:</h4>
-        <input type="radio" v-model="aggregateBy" value="issue" />Issue
-        <input type="radio" v-model="aggregateBy" value="date" />Days
-      </section>
-      <section class="pairing-matrix-config">
-        <h4>Visualise by:</h4>
-        <input type="radio" v-model="chart" value="chord" />Chord Chart
-        <input type="radio" v-model="chart" value="tabular" />Tabular Chart
-      </section>
+    <div class="pairing-matrix-container">
+      <div class="pairing-matrix-config-container">
+        <section class="pairing-matrix-config">
+          <h4>Pull new data from github:</h4>
+          <input type="radio" v-model="pullData" value="true" />Yes
+          <input type="radio" v-model="pullData" value="false" />No
+        </section>
+        <section class="pairing-matrix-config">
+          <h4>For last days:</h4>
+          <input type="number" v-model="sinceDays" />
+        </section>
+        <section class="pairing-matrix-config">
+          <h4>Aggregate By:</h4>
+          <input type="radio" v-model="aggregateBy" value="issue" />Issue
+          <input type="radio" v-model="aggregateBy" value="date" />Days
+        </section>
+        <section class="pairing-matrix-config">
+          <h4>Visualise by:</h4>
+          <input type="radio" v-model="chart" value="chord" />Chord Chart
+          <input type="radio" v-model="chart" value="tabular" />Tabular Chart
+        </section>
+      </div>
+      <PairingMatrix
+        :aggregate-by="aggregateBy"
+        :pull-data="shouldPullData"
+        :since-days="sinceDays"
+        :chart="chart"
+        class="pairing-matrix"
+      ></PairingMatrix>
     </div>
-    <br />
-    <PairingMatrix
-      :aggregate-by="aggregateBy"
-      :pull-data="shouldPullData"
-      :since-days="sinceDays"
-      :chart="chart"
-      class="pairing-matrix"
-    ></PairingMatrix>
   </div>
 </template>
 
@@ -60,17 +61,26 @@ export default {
   width: 80%;
   margin: 0 auto;
 }
+
 .pairing-matrix-title {
   text-align: center;
 }
+
+.pairing-matrix-container {
+  display: flex;
+}
+
 .pairing-matrix-config-container {
   display: flex;
+  flex-direction: column;
   justify-content: center;
 }
+
 .pairing-matrix-config {
   margin-left: 20px;
   margin-right: 20px;
 }
+
 .pairing-matrix {
   width: 65%;
   margin: 0 auto;
